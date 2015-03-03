d5ead47dc82b5af2c9f81a201f8d729cfd51dd7e
Sébastien Muller <igniweb.net@gmail.com>
2015-03-03 01:06:24 +0100

    Add options and events to handle grouping

--- index.php
+++ index.php
@@ -1,20 +1,55 @@
 <!doctype html>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Sandbox / Ammap</title>
     <link rel="stylesheet" type="text/css" href="./assets/css/ammap.css">
 </head>
 <body>
     <div id="map" style="width: 1024px; height: 768px; margin: 30px;"></div>
     <script type="text/javascript" src="./assets/js/ammap.js"></script>
-    <script type="text/javascript" src="./assets/js/maps/worldLow.js"></script>
+    <script type="text/javascript" src="./assets/js/maps/worldHigh.js"></script>
+    <script type="text/javascript" src="./assets/js/lang/fr.js"></script>
     <script type="text/javascript">
         AmCharts.ready(function () {
             var map = AmCharts.makeChart('map', {
                 type: 'map',
+                language: 'fr',
                 pathToImages: './assets/medias/ammap/',
                 dataProvider: {
-                    map: 'worldLow'
+                    map: 'worldHigh',
+                    areas: [
+                        {
+                            id: 'FR',
+                            groupId: 'europe',
+                            showAsSelected: true
+                        },
+                        {
+                            id: 'DE',
+                            groupId: 'europe',
+                            showAsSelected: true
+                        },
+                        {
+                            id: 'BR',
+                            groupId: 'south-america'
+                        },
+                        {
+                            id: 'BO',
+                            groupId: 'south-america'
+                        }
+                    ]
+                },
+                areasSettings: {
+                    autoZoom: true,
+                    unlistedAreasOutlineAlpha: 0.5
+                },
+                zoomControl: {
+                    zoomControlEnabled: false
+                },
+                mouseWheelZoomEnabled: false,
+                zoomOnDoubleClick: false,
+                clickMapObject: function (mapObject) {
+                    this.zoomToGroup(mapObject.groupId);
                 }
             });
         });
